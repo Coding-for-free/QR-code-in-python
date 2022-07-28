aa2231e3619bbbb98632c5e7c57abceaf6db8e1b
Travinth Dayalaeaswaran <Info@codingforfree.com>
2022-07-28 17:10:05 +0530

    Add files via upload

---
+++ Qr.py
@@ -0,0 +1,12 @@
+
+import pyqrcode
+import png
+from pyqrcode import QRCode
+  
+s = "www.codingforfree.com"
+  
+url = pyqrcode.create(s)
+  
+url.svg("qr.svg", scale = 8)
+  
+url.png('qr.png', scale = 6)
